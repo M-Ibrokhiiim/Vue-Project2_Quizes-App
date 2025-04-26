3f77f097ac3eadb15b4d94ba38b0af5c09933ca3
M_Ibrokhim <ibrokhimmuysinov@gmail.com>
2025-04-26 12:39:56 +0300

    50% Done!

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,23 @@
     </div>
   </header>
 
-  <main class="bg-slate-400 w-[80%] m-auto mt-[1vw] ">
-    <section>
-      <div class="bg-slate-100 w-[150px] shadow-xl shadow-black  rounded-[0.4vw] hover:-translate-y-1 hover:opacity-85 active:opacity-100 cursor-pointer active:scale-90 transition-all duration-400 delay-75 ">
-        <img src="./assets/pics/programmingPic.jpg" class="w-[100%]  h-[200px] rounded-[0.4vw] overflow-scroll">
-        <h3 class="text-[1.4vw] font-bold font-sans mb-0 ml-[0.3vw]">Programming</h3>
-        <p class="text-[1vw] ml-[0.3vw] -mt-[0.3vw]">3 questions</p>
-      </div>
+  <main class=" w-[80%]  m-auto mt-[1vw]  items-center">
+    <section class="flex  justify-between flex-wrap ">
+      <div class="bg-slate-100 w-[230px] h-[370px] shadow-xl ml-4 mr-2 shadow-black mt-[3vw] rounded-[0.4vw] hover:-translate-y-1 hover:opacity-85 active:opacity-100 cursor-pointer active:scale-90 transition-all duration-400 delay-75 ">
+        <img src="./assets/pics/programmingPic.jpg" class=" w-[100%] h-[300px] rounded-[0.4vw] overflow-scroll">
+        <h3 class="text-[170%] font-bold font-sans mb-0 ml-[0.3vw]">Programming</h3>
+        <p class="text-[100%] ml-[0.3vw] -mt-[0.3vw]">3 questions</p>
+      </div> 
+      <div class="bg-slate-100 w-[230px] h-[370px] shadow-xl ml-4 mr-2 shadow-black mt-[3vw] rounded-[0.4vw] hover:-translate-y-1 hover:opacity-85 active:opacity-100 cursor-pointer active:scale-90 transition-all duration-400 delay-75 ">
+        <img src="./assets/pics/mathPic.jpg" class=" w-[100%] h-[300px] rounded-[0.4vw] overflow-scroll">
+        <h3 class="text-[170%] font-bold font-sans mb-0 ml-[0.3vw]">Mathematical</h3>
+        <p class="text-[100%] ml-[0.3vw] -mt-[0.3vw]">3 questions</p>
+      </div> 
+      <div class="bg-slate-100 w-[230px] h-[370px] shadow-xl ml-4 mr-2 shadow-black mt-[3vw] rounded-[0.4vw] hover:-translate-y-1 hover:opacity-85 active:opacity-100 cursor-pointer active:scale-90 transition-all duration-400 delay-75 ">
+        <img src="./assets/pics/businessPic.jpg" class=" w-[100%] h-[300px] rounded-[0.4vw] overflow-scroll">
+        <h3 class="text-[170%] font-bold font-sans mb-0 ml-[0.3vw]">Business</h3>
+        <p class="text-[100%] ml-[0.3vw] -mt-[0.3vw]">3 questions</p>
+      </div>  
     </section>
   </main>
 </template>
